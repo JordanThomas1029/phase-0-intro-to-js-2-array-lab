e76f8f40500ca6b3201b9969333353c90f7f6324
Jordan Thomas <tjordan706@gmail.com>
2021-06-15 22:30:38 -0400

    complete assignment

--- index.js
+++ index.js
@@ -2,42 +2,40 @@
 
 const cats = ["Milo", "Otis", "Garfield"];
 
-
-
-function destructivelyAppendCat(name){
-    cats.push("Ralph");
-    return cats;
+function destructivelyAppendCat(name) {
+  cats.push("Ralph");
+  return cats;
 }
 
-function destructivelyPrependCat(name){
-    cats.unshift("Bob");
-    return cats;
+function destructivelyPrependCat(name) {
+  cats.unshift("Bob");
+  return cats;
 }
 
-function destructivelyRemoveFirstCat(name){
-    cats.shift("Bob");
-    return cats;
+function destructivelyRemoveFirstCat(name) {
+  cats.shift("Bob");
+  return cats;
 }
 
-function destructivelyRemoveLastCat(name){
-    cats.pop("Garfield");
-    return cats;
+function destructivelyRemoveLastCat(name) {
+  cats.pop("Garfield");
+  return cats;
 }
 
-function prependCat(name){
-    const coolCats = ["Arnold",...cats,];
-    return coolCats;
+function prependCat(name) {
+  const coolCats = ["Arnold", ...cats];
+  return coolCats;
 }
 
-function appendCat(name){
-    const newCats = [...cats, "Broom"];
-    return newCats;
+function appendCat(name) {
+  const newCats = [...cats, "Broom"];
+  return newCats;
 }
 
-function removeFirstCat(name){
-   return cats.slice(1);
+function removeFirstCat(name) {
+  return cats.slice(1);
 }
 
-function removeLastCat(name){
-    return cats.slice(0, -1);
+function removeLastCat(name) {
+  return cats.slice(0, -1);
 }
